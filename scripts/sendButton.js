55eda7a23ed87842fb133d1af703bff51a43ef55
Nicolas Lundin <nicolas.lundin@the-cocktail.com>
2019-09-26 17:59:44 -0300

    get weekdays array

--- scripts/sendButton.js
+++ scripts/sendButton.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
+  // disable send button
   $('#sendButton').attr('disabled',true);
-
+  // enable send button if input has value
   $('.inputRequired').keyup(function(){
     if($(this).val().length !==0) {
       $('#sendButton').attr('disabled', false);
@@ -8,7 +9,7 @@ $(document).ready(function(){
       $('#sendButton').attr('disabled', true);
     }
   });
-
+  // enable send button if option selected
   $('.optionRequired').click(function() {
     if($(this).is(':checked')) {
         $('#sendButton').attr('disabled', false);
